cff424c8147529d6102d0147c28748c03163a5c4
John <jparkteach@yahoo.com>
2017-03-27 16:53:09 -0400

    TakeHomeAssignment08-JohnP

--- app/src/main/java/net/jptechnology/android/takehomeassignment08_johnp/MainActivity.java
+++ app/src/main/java/net/jptechnology/android/takehomeassignment08_johnp/MainActivity.java
@@ -1,5 +1,7 @@
 package net.jptechnology.android.takehomeassignment08_johnp;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
@@ -10,7 +12,20 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     private View imageContainer;
     private TextView overlayText;
@@ -29,11 +44,12 @@ public class MainActivity extends AppCompatActivity {
         overlayText.setVisibility(View.INVISIBLE);
         EditText textInput = (EditText) findViewById(R.id.text_input);
         textInput.addTextChangedListener(new InputTextWatcher());
-//        uploadButton = (Button) findViewById(R.id.upload_button);
-//        uploadButton.setOnClickListener(new UploadOnClickListener());
-//        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
-//        progressBar.setVisibility(View.GONE);
-//        downloadUrl = (TextView) findViewById(R.id.download_url);
+        uploadButton = (Button) findViewById(R.id.upload_button);
+        uploadButton.setOnClickListener(new UploadOnClickListener());
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.GONE);
+        downloadUrl = (TextView) findViewById(R.id.download_url);
+        downloadUrl.setVisibility(View.INVISIBLE);
     }
 
     private class InputTextWatcher implements TextWatcher {
@@ -52,6 +68,43 @@ public class MainActivity extends AppCompatActivity {
         public void afterTextChanged(Editable s) {
             overlayText.setVisibility(s.length() > 0 ? View.VISIBLE : View.INVISIBLE);
             overlayText.setText(s.toString());
+        }
+    }
+
+    private class UploadOnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            imageContainer.setDrawingCacheEnabled(true);
+            imageContainer.buildDrawingCache();
+            Bitmap bitmap = imageContainer.getDrawingCache();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            imageContainer.setDrawingCacheEnabled(false);
+            byte[] data = baos.toByteArray();
+
+            String path = "userpics/" + UUID.randomUUID() + ".png";
+            StorageReference userpicRef = storage.getReference(path);
+
+            StorageMetadata metadata = new StorageMetadata.Builder()
+                    .setCustomMetadata("text", overlayText.getText().toString())
+                    .build();
+
+            progressBar.setVisibility(View.VISIBLE);
+            uploadButton.setEnabled(false);
+
+            UploadTask uploadTask = userpicRef.putBytes(data, metadata);
+            uploadTask.addOnSuccessListener(MainActivity.this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    progressBar.setVisibility(View.GONE);
+                    uploadButton.setEnabled(true);
+
+                    Uri url = taskSnapshot.getDownloadUrl();
+                    downloadUrl.setText(url.toString());
+                    downloadUrl.setVisibility(View.VISIBLE);
+                }
+            });
         }
     }
 }
